9851b0b7e73d6f8096c81d987a3a62879d00b44d
ont <ont.rif@gmail.com>
2018-08-02 20:48:44 +0700

    (+) jaeger opentracing

--- client/client.go
+++ client/client.go
@@ -3,16 +3,19 @@ package client
 import (
 	"bytes"
 	"fmt"
-	"github.com/kataras/iris/context"
-	"github.com/ont/iris-related/logging"
-	"github.com/ont/iris-related/requestid"
-	"github.com/sirupsen/logrus"
-	"io"
 	"io/ioutil"
 	"net/http"
 	urllib "net/url"
 	"path"
 	"time"
+
+	gocontext "context"
+
+	iriscontext "github.com/kataras/iris/context"
+	"github.com/ont/iris-related/logging"
+	"github.com/ont/iris-related/requestid"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
 )
 
 // Simple buffered client which returns strings and follows redirects.
@@ -20,18 +23,30 @@ import (
 type Client struct {
 	BaseURL string // base url for doing request to (example "http://some-site.com")
 
-	ctx       context.Context // current iris context
-	requestId string          // request-id extracted from context
-	log       *logrus.Entry   // logger extracted from context
+	ctx       iriscontext.Context // current iris context
+	requestId string              // request-id extracted from context
+	log       *logrus.Entry       // logger extracted from context
+
+	traceCtx gocontext.Context
 }
 
-func NewClient(ctx context.Context, baseUrl string) *Client {
+func NewClient(baseUrl string) *Client {
 	return &Client{
-		BaseURL:   baseUrl,
-		ctx:       ctx,
-		requestId: requestid.Get(ctx),
-		log:       logging.Get(ctx),
+		BaseURL: baseUrl,
 	}
+}
+
+func (c *Client) WithIris(ctx iriscontext.Context) *Client {
+	c.ctx = ctx
+	c.requestId = requestid.Get(ctx)
+	c.log = logging.Get(ctx)
+
+	return c
+}
+
+func (c *Client) WithTrace(traceCtx gocontext.Context) *Client {
+	c.traceCtx = traceCtx
+	return c
 }
 
 // simple version of GET which only returns response body for 2xx response codes (and follows redirects)
@@ -41,7 +56,7 @@ func (c *Client) GET(url string) (string, error) {
 		return "", err
 	}
 
-	return c.doRequest("GET", url, nil, nil)
+	return c.doRequest("GET", url, "", nil)
 }
 
 // simple version of POST for sending ...
@@ -57,21 +72,35 @@ func (c *Client) POST(url string, data map[string]interface{}) (string, error) {
 		form.Add(k, fmt.Sprintf("%v", v))
 	}
 
-	return c.doRequest("POST", url, bytes.NewBufferString(form.Encode()), map[string]string{
+	return c.doRequest("POST", url, form.Encode(), map[string]string{
 		"Content-Type": "application/x-www-form-urlencoded",
 	})
 }
 
-func (c *Client) doRequest(method string, url string, data io.Reader, headers map[string]string) (string, error) {
+func (c *Client) doRequest(method string, url string, data string, headers map[string]string) (string, error) {
+	var span opentracing.Span
+
+	if c.traceCtx != nil {
+		span, _ = opentracing.StartSpanFromContext(c.traceCtx, "doRequest")
+		defer span.Finish()
+	}
+
 	c.log.WithField("http_method", method).Debugf("Request to %s", url)
 
-	req, err := http.NewRequest(method, url, data)
+	var buffer *bytes.Buffer
+	if data != "" {
+		buffer = bytes.NewBufferString(data)
+	}
+
+	req, err := http.NewRequest(method, url, buffer)
 	if err != nil {
 		return "", err
 	}
 
 	// TODO: headers for json (separate method doJsonRequest?)
-	req.Header.Set("X-Request-Id", c.requestId) // add Request-Id for each request
+	if c.requestId != "" {
+		req.Header.Set("X-Request-Id", c.requestId) // add Request-Id for each request
+	}
 
 	for name, value := range headers {
 		req.Header.Add(name, value)
@@ -81,18 +110,59 @@ func (c *Client) doRequest(method string, url string, data io.Reader, headers ma
 		Timeout: 10 * time.Second, // NOTE: very important (default timeout is infinite)
 	}
 
+	// log info about request and inject span into HTTP headers
+	if span != nil {
+		span.SetTag("method", method).
+			SetTag("url", url).
+			LogKV(
+				"event", "doing request",
+				"method", method,
+				"url", url,
+				"data", data,
+			)
+
+		opentracing.GlobalTracer().Inject(
+			span.Context(),
+			opentracing.HTTPHeaders,
+			opentracing.HTTPHeadersCarrier(req.Header),
+		)
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
+
+		if span != nil {
+			span.SetTag("error", true).
+				LogKV(
+					"event", "error during http request",
+					"error", err.Error(),
+				)
+		}
 		return "", err
 	}
 
 	defer resp.Body.Close()
+
+	if span != nil {
+		span.LogKV(
+			"event", "response from server",
+			"status", resp.Status,
+			"headers", resp.Header,
+		)
+	}
 	c.log.Debug("response Status: ", resp.Status)
 	c.log.Debug("response Headers: ", resp.Header)
 
 	bytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
+	}
+
+	if span != nil {
+		span.LogKV(
+			"event", "response body from server",
+			"body", string(bytes),
+		)
 	}
 
 	c.log.Debug("response Body: ", string(bytes))
---
+++ opentracing/opentracing.go
@@ -0,0 +1,89 @@
+package opentracing
+
+import (
+	"io"
+	"log"
+	"runtime/debug"
+
+	gocontext "context"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+	opentracing "github.com/opentracing/opentracing-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+)
+
+var (
+	tracer opentracing.Tracer
+	closer io.Closer
+)
+
+func GetContextFrom(ctx context.Context) gocontext.Context {
+	return ctx.Values().Get("opentrace-ctx").(gocontext.Context)
+}
+
+func StartSpanFromContext(ctx context.Context, spanName string) (opentracing.Span, gocontext.Context) {
+	tctx := GetContextFrom(ctx)
+	return opentracing.StartSpanFromContext(tctx, spanName)
+}
+
+func Middleware(ctx context.Context) {
+	carrier := opentracing.HTTPHeadersCarrier(ctx.Request().Header)
+	spanCtx, err := tracer.Extract(opentracing.HTTPHeaders, carrier)
+
+	var span opentracing.Span
+	if err != nil {
+		span = tracer.StartSpan("got request")
+	} else {
+		span = tracer.StartSpan("got request", opentracing.ChildOf(spanCtx))
+	}
+
+	if err != opentracing.ErrSpanContextNotFound {
+		span.SetTag("error", true)
+		span.LogKV(
+			"event", "error",
+			"error", err.Error(),
+		)
+		ctx.StatusCode(iris.StatusBadRequest)
+		ctx.StopExecution()
+	}
+
+	traceCtx := opentracing.ContextWithSpan(gocontext.Background(), span)
+
+	ctx.Values().Set("opentrace-ctx", traceCtx)
+
+	span.SetTag("path", ctx.Path()).
+		SetTag("method", ctx.Method())
+
+	defer func() {
+		if r := recover(); r != nil {
+			span.SetTag("panic", true).
+				SetTag("error", true).
+				SetTag("panic-message", r).
+				LogKV("trace", string(debug.Stack()))
+		}
+
+		span.Finish()
+	}()
+
+	ctx.Next()
+}
+
+func NewTracerFromEnv() (opentracing.Tracer, io.Closer) {
+	cfg, err := jaegercfg.FromEnv()
+	if err != nil {
+		log.Fatalf("Can't parse jaeger config from env vars: %s", err)
+	}
+
+	tracer, closer, err := cfg.NewTracer()
+	if err != nil {
+		log.Fatalf("Can't init jaeger tracing: %s", err)
+	}
+
+	return tracer, closer
+}
+
+func init() {
+	tracer, closer = NewTracerFromEnv()
+	opentracing.SetGlobalTracer(tracer)
+}
